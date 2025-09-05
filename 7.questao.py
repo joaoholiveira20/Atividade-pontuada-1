03b8923adc14df966b20113086e0f8dccac323b0
joaoholiveira20 <joao.h.oliveira@ba.estudante.senai.br>
2025-09-05 23:28:10 +0000

    fix: corrigir o código

--- 7.questao.py
+++ 7.questao.py
@@ -14,14 +14,14 @@ if quantidade <= 5:
     total_desconto = desconto
 elif quantidade >5 and quantidade <= 10:
     desconto = quantidade + preco * 0.03
-    total_desconto = desconto
+    total_desconto = desconto - preco
 else:
     desconto = quantidade + preco * 0.05
-    total_desconto = desconto
+    total_desconto = desconto - preco
 
 total = quantidade * preco
-total_com_desconto = total 
+total_com_desconto = total - total_desconto
 
 print(f"total da compra: {total}")
 print(f"o desconto: {desconto}")
-print(f"total a pagar: {total_desconto}")
+print(f"total a pagar: {total_com_desconto}")
